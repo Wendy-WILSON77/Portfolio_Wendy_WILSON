9f222ea6569aae33e8290cda8841cee72911fb30
Wendy-WILSON77 <164891253+Wendy-WILSON77@users.noreply.github.com>
2024-08-11 16:55:34 +0200

    refonte

--- public/otherjs/other.js
+++ public/otherjs/other.js
@@ -1,31 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Le fichier JavaScript est bien connecté.');
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log('Le fichier JavaScript est bien connecté.');
 
-    const dropdown = document.querySelector('#navbar1 .dropdown-1');
-    const button = dropdown.querySelector('.button3');
-    const contentMenu = dropdown.querySelector('.contentMenuD');
+//     const dropdown = document.querySelector('#navbar1 .dropdown-1');
+//     // const button = dropdown.querySelector('.button3');
+//     // const contentMenu = dropdown.querySelector('.contentMenuD');
 
-    button.addEventListener('click', function(event) {
-        event.stopPropagation();
-        dropdown.classList.add('active');
-    });
+//    document.addEventListener('click', function(event) {
+//         event.stopPropagation();
+//         dropdown.classList.add('active');
+//     });
 
-    document.addEventListener('click', function(event) {
-        if (!dropdown.contains(event.target)) {
-            dropdown.classList.remove('active');
-        }
-    });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
+//     document.addEventListener('click', function(event) {
+//         if (!dropdown.contains(event.target)) {
+//             dropdown.classList.remove('active');
+//         }
+//     });
+// });
