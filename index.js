2cc6c43f7d66d85276a718e258585522e0ce1a57
Wendy-WILSON77 <164891253+Wendy-WILSON77@users.noreply.github.com>
2024-08-19 21:48:06 +0200

    essai suite interv

--- index.js
+++ index.js
@@ -1,30 +1,74 @@
+
 require('dotenv').config();
 
 // Import des dépendances tierses
 const express = require('express');
+const path = require('path');
 // Import des dépendances locales
 const router = require ('./app/router');
 
 // Create Express app
 const app = express();
 // Configure static
-app.use(express.static('./public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Configure view engine
 app.set('view engine','ejs');
-app.set('views','./app/views');
-
+app.set('views', path.join(__dirname, 'app/views'));
 
 // Configure routes
 app.use(router);
 
-
-
-
-
-const port = process.env.PORT || 4000
-const host = '0.0.0.0';
-app.listen(port, host, () => {
-  console.log('Serveur OK, lancé sur http://localhost:4000 ');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serveur OK, lancé sur http://localhost:${port} `);
 });
+
+module.exports = app
+
+
+
+
+
+
+
+
+
+
+
+// AVANT DEPLOY
+
+
+
+
+
+// require('dotenv').config();
+
+// // Import des dépendances tierses
+// const express = require('express');
+// // Import des dépendances locales
+// const router = require ('./app/router');
+
+// // Create Express app
+// const app = express();
+// // Configure static
+// app.use(express.static('./public'));
+
+
+// // Configure view engine
+// app.set('view engine','ejs');
+// app.set('views','./app/views');
+
+
+// // Configure routes
+// app.use(router);
+
+
+
+
+
+// const port = process.env.PORT || 4000
+// const host = '0.0.0.0';
+// app.listen(port, host, () => {
+//   console.log('Serveur OK, lancé sur http://localhost:4000 ');
+// });
